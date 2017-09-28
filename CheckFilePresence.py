813f0cd3e60070108803b13c427bddc94cb40635
drberg1000 <stay0ut>
2017-09-27 22:23:12 -0500

    Switch from strings to lxml.  Refactor. Add Tests for more robust constructor.  Red Light.

--- CheckFilePresence.py
+++ CheckFilePresence.py
@@ -1,4 +1,5 @@
 # import requests
+from lxml import etree
 
 
 class Clarity:
@@ -8,9 +9,13 @@ class Clarity:
         self.base_uri = 'https://' + host + '/api/v2/'
 
     def create_batch_xml_for_post(self, luids, link_type):
-        xml = '<ri:links xmlns:ri="http://genologics.com/ri">'
-        for luid in luids:
-            xml += '<link uri="' + self.base_uri + 'artifact/' + luid + '" rel="' + link_type + '"/>'
-        xml += '</ri:links>'
+        nsmap = {'ri': 'http://genologics.com/ri'}
+        dom = etree.Element('{%s}links' % nsmap['ri'], nsmap=nsmap)
 
-        return xml
+        for luid in luids:
+            link = etree.Element('link')
+            link.attrib['uri'] = self.base_uri + 'artifact/' + luid
+            link.attrib['rel'] = link_type
+            dom.append(link)
+
+        return etree.tostring(dom, pretty_print=True).decode('utf8')
--- test_clarity.py
+++ test_clarity.py
@@ -12,3 +12,26 @@ class TestClarity(TestCase):
         self.assertIn('<ri:links xmlns:ri="http://genologics.com/ri">', xml)
         self.assertIn('92-164026', xml)
         self.assertIn('<link uri="https://roflms801a.mayo.edu/api/v2/artifact/92-164028"', xml)
+
+    def test___init___fqdn(self):
+        clarity = Clarity('roflms801a.mayo.edu')
+
+        self.assertEqual(
+            clarity.base_uri, 'https://roflms801a.mayo.edu/api/v2/',
+            "It should properly generate base URI when a FQDN is passed."
+        )
+
+    def test___init___good_uri(self):
+        clarity = Clarity('https://roflms801a.mayo.edu/api/v2')
+
+        self.assertEqual(
+            clarity.base_uri, 'https://roflms801a.mayo.edu/api/v2/step/24-12345/details',
+            "It should extract the base uri from a full uri."
+        )
+
+    def test___init___bad_uri(self):
+        with self.assertRaisesRegex(
+                ValueError,
+                "Invalid format for URI. Should be 'https://roflms801a.mayo.edu/api/v2[/endpoint]"
+        ):
+            clarity = Clarity('https://roflms801a.mayo.edu/api/v2')
